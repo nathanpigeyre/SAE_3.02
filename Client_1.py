6813b4145037de5c8317d21621e02e7fa8685464
Nathan Pigeyre <nathan.pigeyre@uha.fr>
2023-11-12 13:38:55 +0100

    Le Client peut envoyer des messages au serveur

--- Client_1.py
+++ Client_1.py
@@ -7,41 +7,38 @@ def receive_messages(client_socket):
         try:
             reply = client_socket.recv(1024).decode()
             if not reply:
-                Connected = False
+                print("Connexion perdue avec le serveur")
+                break
             print("\nMessage (autre Client): " + reply)
         except ConnectionResetError:
+            print("Connexion perdue avec le serveur")
             break
+
 def main():
     host = "127.0.0.1"
-    port = 1500
-    Boucle = True
+    port = 1501
+
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    Connected = True
+    while Connected == True:
+        try:
+            client_socket.connect((host, port))
+            print("Connecté au serveur")
+            receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+            receive_thread.start()
+            break
+        except ConnectionRefusedError:
+            print("Le serveur n'est pas connecté")
+        except OSError:
+            break
 
-    while Boucle == True:
-        Connected = False
-        while Connected == False:
-            try:
-                client_socket.connect((host, port))
-            except ConnectionRefusedError:
-                print("Le Server n'est pas connecté")
-            except OSError:
-                break
-            else:
-                Connected = True
-                print("Connecter au serveur")
-                receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
-                receive_thread.start()
-        if Connected == True:
-            message_send = str(input("Vous : "))
-            try:
-                client_socket.send(message_send.encode())
-            except ConnectionAbortedError:
-                print("Le serveur n'est pas connecté")
-                connected = False
-            else:
-                print("")#pas encore fait
-
+    while Connected == True:
+        message_send = str(input("Vous : "))
+        try:
+            client_socket.send(message_send.encode())
+        except ConnectionAbortedError:
+            print("Le serveur n'est pas connecté")
+            break
 
 if __name__ == "__main__":
     main()
-
--- Serveur.py
+++ Serveur.py
@@ -2,18 +2,16 @@ import socket
 import threading
 
 clients = []
+
 def main():
     host = "127.0.0.1"
-    port = 1500
-
+    port = 1501
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen(5)
     print("Le Serveur écoute {}:{}".format(host, port))
-
     Connected = True
-
     while Connected == True:
         client_socket, client_address = server_socket.accept()
         print("Accepte la connection de  {}".format(client_address[0]))
@@ -27,27 +25,19 @@ def server(server_socket):
         try:
             message_receve = server_socket.recv(1024).decode()
             if not message_receve:
-                Connected = False
-            print("\nMessage (autre Client): " + message_receve)
+                break
+            print("\nMessage (Client): " + message_receve)
             
             for pointeur in clients:
-                    if pointeur != server_socket:
-                        try:
-                            pointeur.send(message_receve.encode())
-                        except ConnectionResetError:
-                            pass
+                if pointeur != server_socket:
+                    try:
+                        pointeur.send(message_receve.encode())
+                    except ConnectionResetError:
+                        pass
         except ConnectionResetError:
+            print("Client déconnecté")
+            clients.remove(server_socket)
             break
-        except ConnectionAbortedError:
-            break
-        except OSError:
-            exit()
-        except AttributeError:
-            exit()
-
-
-
-
 
 if __name__ == "__main__":
     main()
