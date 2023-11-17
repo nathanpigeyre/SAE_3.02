d135a462d889895c358155724b490b964d58bf6e
Nathan Pigeyre <nathan.pigeyre@uha.fr>
2023-11-17 15:57:40 +0100

    changement mineur

--- Client_2.py
+++ Client_2.py
@@ -14,15 +14,19 @@ def receive_messages(client_socket):
             break
 
 def main():
-    host = "127.0.0.1"
+    ip_address = "127.0.0.1"
     port = 1501
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((host, port))
+    client_socket.connect((ip_address, port))
 
     # Inscription
     username = input("Ton Nom: ")
+    password = input("Ton Mot de Passe: ")
+
     client_socket.send(username.encode())
+    client_socket.send(password.encode())  # Envoyer le mot de passe au serveur
+
     registration_status = client_socket.recv(1024).decode()
     print(registration_status)
 
--- Serveur.py
+++ Serveur.py
@@ -1,34 +1,46 @@
 import socket
 import threading
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="toto",
+    database="discord_db"
+)
+
+cursor = db.cursor()
 
 clients = {}
-nom = set()
 
 def main():
-    host = "127.0.0.1"
+    ip_address = "127.0.0.1"
     port = 1501
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((host, port))
+    server_socket.bind((ip_address, port))
     server_socket.listen(5)
-    print("Le Serveur écoute {}:{}".format(host, port))
+    print("Le Serveur écoute {}:{}".format(ip_address, port))
+
     Connected = True
     while Connected == True:
         client_socket, client_address = server_socket.accept()
         print("Accepte la connection de {}".format(client_address[0]))
+        print(client_socket)
         threading.Thread(target=Server, args=(client_socket,)).start()
 
 def Server(client_socket):
     id = client_socket.recv(1024).decode()
-    if id in nom:
-        client_socket.send("L'utilisateur es déja pris. Déconnection...".encode())
-        client_socket.close()
-        return
-    else:
-        nom.add(id)
-        clients[client_socket] = id
-        client_socket.send("Enregistrement est un succès. Vous pouvez envoyer des messages.".encode())
-        print("Bienvenue à {}".format(id))
+    password = client_socket.recv(1024).decode()
+
+    cursor.execute("INSERT INTO users (nom, password, ip_address) VALUES (%s, %s, %s)", (id, password, client_socket.getpeername()[0]))
+    db.commit()
+
+    client_socket.send("Enregistrement est un succès. Vous pouvez envoyer des messages.".encode())
+    print("Bienvenue a toi : {}".format(id))
+
+    clients[client_socket] = id
+
     Connected = True
     while Connected == True:
         try:
@@ -40,14 +52,14 @@ def Server(client_socket):
             for socket, name in clients.items():
                 if socket != client_socket:
                     try:
-
-                        socket.send("\n".encode())
+                        socket.send("\n")
                         socket.send("{}: {}".format(id, message_receive).encode())
                     except ConnectionResetError:
                         pass
         except ConnectionResetError:
             print("{} disconnected".format(id))
-            nom.remove(id)
+            cursor.execute("DELETE FROM users WHERE nom = %s", (id,))
+            db.commit()
             del clients[client_socket]
             break
 
